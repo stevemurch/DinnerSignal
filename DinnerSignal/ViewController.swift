bc384fac8d8833d31a501f526801f0f9fb3beb61
Steve Murch <ceo@bigoven.com>
2018-01-11 11:17:38 -0800

    Initial Commit

---
+++ DinnerSignal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DinnerSignal
+//
+//  Created by Steve Murch on 1/11/18.
+//  Copyright © 2018 Steve Murch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
